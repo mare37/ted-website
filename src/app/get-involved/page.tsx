c5e8606189d87cee9ad717bcca04132c05fe1321
unknown <marewilson35@gmail.com>
2023-07-13 17:43:56 +0300

    why-page-build

--- src/app/get-involved/page.tsx
+++ src/app/get-involved/page.tsx
@@ -1,8 +1,24 @@
+import "./page.modules.css"
+import Hero2 from "./Hero/Hero";
+
 
 function GetInvolved (){
 
 
-    return <div>GetInvolved</div>
+    return <div   className="get-involved">
+
+        <Hero2/>
+
+        <div>
+
+            <p>
+            Dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend
+
+            </p>
+
+        </div>
+
+    </div>
 }
 
 export default GetInvolved ;
