db730ab1873e01b94d4c1fae70600ad8ef7f4bee
unknown <marewilson35@gmail.com>
2023-07-17 15:41:49 +0300

    issue/11

---
+++ src/app/impact/Hero/Hero.tsx
@@ -0,0 +1,24 @@
+import "./Hero.modules.css"
+
+
+export default function Hero3() {
+    return (
+     <div className="hero3"  >
+
+      <div className="hero-container3"  >
+
+        <p>Our Impact In the Field and Stories as Narrated by Locals and Partners </p>
+
+       
+    
+      </div>
+
+
+     
+        
+
+
+     </div>
+    )
+  }
+  
