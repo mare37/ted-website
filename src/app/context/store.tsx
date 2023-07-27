38cbbdb1a8c9172fc3dc165463256b2786707f75
unknown <marewilson35@gmail.com>
2023-07-27 20:08:31 +0300

    issue/29

--- src/app/context/store.tsx
+++ src/app/context/store.tsx
@@ -8,6 +8,8 @@ import { createContext ,SetStateAction,useState,Dispatch, useContext} from "reac
 interface ContextProps {
     sidebar: boolean,
     setSidebar: Dispatch<SetStateAction<boolean>>
+    popup: boolean,
+    setPopup: Dispatch<SetStateAction<boolean>>
 }
 
 
@@ -15,18 +17,21 @@ interface ContextProps {
 
 const GlobalContext = createContext<ContextProps>({
     sidebar: false,
-    setSidebar: (): boolean => false
+    setSidebar: (): boolean => false,
+    popup: false,
+    setPopup: (): boolean => false
 
 })
 
 
 export const GlobalContextProvider = ({children})=>{
     const [sidebar, setSidebar] = useState(false);
+    const [popup, setPopup] = useState(false);
 
 
 
     return (
-        <GlobalContext.Provider  value={{sidebar, setSidebar}}>
+        <GlobalContext.Provider  value={{sidebar, setSidebar, popup, setPopup}}>
           {children}
         </GlobalContext.Provider>
     )
