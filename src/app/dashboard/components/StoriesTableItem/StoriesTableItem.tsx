6f7f4d99d55606e2f4c40c4647589e02aecdc6a3
unknown <marewilson35@gmail.com>
2023-08-16 21:17:14 +0300

    issue/55

--- src/app/dashboard/components/StoriesTableItem/StoriesTableItem.tsx
+++ src/app/dashboard/components/StoriesTableItem/StoriesTableItem.tsx
@@ -3,6 +3,7 @@ import "./StoriesTableItem.modules.css"
 import { ObjectId } from "mongoose"
 import { useGlobalContext } from "@/app/context/store";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 
 interface Story {
@@ -21,14 +22,16 @@ interface Story {
 
 function StoriesTableItem({ id,tag, title,story}:Story){
 
+  const router = useRouter()
+
  // console.log(id);
   
 
     const {popup,setPopup, journalId, setJournalId,journalTitle, setJournalTitle,storyId, setStoryId,storyTitle, setStoryTitle } = useGlobalContext();
 
     return  <div className="table-item"  >
-                <span className="table-item-title">{title}</span>
-               <span className="table-item-tag" >{tag}</span>
+                <span   onClick={()=>{router.push(`/impact/${id}`)}}      className="table-item-title">{title}</span>
+               <span   onClick={()=>{router.push(`/impact/${id}`)}}       className="table-item-tag" >{tag}</span>
                <span className="table-item-buttons"> <button  className="tableItem-edit"  >  <Link  href={`/dashboard/stories/editstory/${id.toString()}`}     >Edit</Link>  </button> <button  onClick={()=>{setPopup(true);setStoryId(id.toString()), setStoryTitle(title)  }} className="tableItem-delete">Delete</button>  </span>
             </div>
 }
