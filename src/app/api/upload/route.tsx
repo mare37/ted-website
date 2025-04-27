58b087095349ab244f906bee7da65da46d9bc0ba
Wilson Mare <marewilson35@gmail.com>
2025-04-27 13:45:18 +0300

    remove config

--- src/app/api/upload/route.tsx
+++ src/app/api/upload/route.tsx
@@ -1,138 +1,110 @@
-import { NextRequest } from "next/server"
-import { NextResponse } from "next/server"
+import { NextResponse } from "next/server";
 
-import { buffer } from "stream/consumers"
-import path from "path"
-import { join } from "path"
-import Journals from "@/app/Models/Journals"
-import Stories from "@/app/Models/Stories"
-import locations from "@/app/Models/locations"
+import { join } from "path";
+import Journals from "@/app/Models/Journals";
+import Stories from "@/app/Models/Stories";
+import locations from "@/app/Models/locations";
 
-import { writeFile } from "fs/promises"
-
-
-export const config = {
-    api: {
-      bodyParser: false,
-    },
-  }
+import { writeFile } from "fs/promises";
 
 
 
-  
+export const POST = async (req: Request) => {
+  //
 
+  try {
+    const data = await req.formData();
+    console.log(data);
 
+    const file: File | null = data.get("file") as unknown as File;
 
+    let id: string = data.get("id") as unknown as string;
 
+    const fileName: string = data.get("fileName") as unknown as string;
 
-  export const POST = async (req:Request) =>{
+    let identity: string = data.get("identity") as unknown as string;
 
-  //  
+    console.log(id);
+    console.log(fileName);
 
-    try {
-     
-      const data = await req.formData()
-      console.log(data);
+    if (!file) {
+      return new NextResponse(JSON.stringify({ message: "No file" }), {
+        status: 500,
+      });
+    }
 
-      const file:File | null = data.get("file") as unknown as File
+    const bytes = await file.arrayBuffer();
+    const buffer = Buffer.from(bytes);
 
-      let id : string = data.get("id") as unknown as string
+    const path = join(process.cwd(), "public", file.name);
+    console.log(path);
+    await writeFile(path, buffer);
 
-      const fileName : string = data.get("fileName") as unknown as string
+    console.log(identity);
 
-      let identity : string = data.get("identity") as unknown as string
+    if (identity === "JournalUpload") {
+      const result = await Journals.updateOne(
+        { _id: id },
+        {
+          imageName: fileName,
+        },
+        { strict: false }
+      );
 
+      console.log(result);
 
-      
+      return new NextResponse(
+        JSON.stringify({
+          photoUploaded: true,
+          message: "Successfully uploaded Journal photo",
+        }),
+        { status: 200 }
+      );
+    }
 
+    if (identity === "StoriesUpload") {
+      const result = await Stories.updateOne(
+        { _id: id },
+        {
+          imageName: fileName,
+        },
+        { strict: false }
+      );
 
-      console.log(id);
-      console.log(fileName);
-      
-      
+      console.log(result);
 
-      if(!file){
-        return new NextResponse(JSON.stringify({message:"No file"}), {status:500});
-      }
+      return new NextResponse(
+        JSON.stringify({
+          photoUploaded: true,
+          message: "Successfully uploaded Story photo",
+        }),
+        { status: 200 }
+      );
+    }
 
-      const bytes = await file.arrayBuffer()
-      const buffer  = Buffer.from(bytes)
-
-      const path = join(process.cwd(),'public', file.name)
-      console.log(path);
-      await writeFile(path,buffer)
-
+    if (identity === "locationUpload") {
       console.log(identity);
-      
 
-      if(identity === "JournalUpload"){
+      const result = await locations.updateOne(
+        { _id: id },
+        {
+          imageName: fileName,
+        },
+        { strict: false }
+      );
 
-        const result = await Journals.updateOne(
-            { _id: id  },
-            {
-                imageName:fileName
-           
-             },{strict:false});
-    
-             console.log(result);
-             
-             return new NextResponse(JSON.stringify({photoUploaded:true, message:"Successfully uploaded Journal photo"}), {status:200});
+      console.log(result);
 
-
-      }
-
-      if(identity === "StoriesUpload"){
-
-
-        const result = await Stories.updateOne(
-            { _id: id  },
-            {
-                imageName:fileName
-           
-             },{strict:false});
-    
-             console.log(result);
-             
-             return new NextResponse(JSON.stringify({photoUploaded:true, message:"Successfully uploaded Story photo"}), {status:200});
-
-
-      }
-
-
-      if(identity === "locationUpload"){
-
-        console.log(identity);
-        
-
-        const result = await locations.updateOne(
-            { _id: id  },
-            {
-                imageName:fileName
-           
-             },{strict:false});
-    
-             console.log(result);
-             
-             return new NextResponse(JSON.stringify({photoUploaded:true, message:"Successfully uploaded Location photo"}), {status:200});
-
-
-      }
-
-      } catch (e) {
-        console.log(e)
-        return new Response("fail")
-      }
-
-   
-    
-
-   
-
-
-   
-
-
-
-
-
+      return new NextResponse(
+        JSON.stringify({
+          photoUploaded: true,
+          message: "Successfully uploaded Location photo",
+        }),
+        { status: 200 }
+      );
+    }
+  } catch (e) {
+    console.log(e);
+    return new Response("fail");
   }
+};
